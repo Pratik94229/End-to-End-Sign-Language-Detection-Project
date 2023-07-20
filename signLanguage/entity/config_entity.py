23f811d7f167ee737ae926300d07306b7ca8f877
Pratik94229 <pratik.941@gmail.com>
2023-07-20 17:04:48 +0530

    model created and loaded

---
+++ signLanguage/entity/config_entity.py
@@ -0,0 +1,62 @@
+import os
+from dataclasses import dataclass
+from datetime import datetime
+from signLanguage.constant.training_pipeline import *
+
+TIMESTAMP: str = datetime.now().strftime("%m_%d_%Y_%H_%M_%S")
+
+@dataclass
+class TrainingPipelineConfig:
+    artifacts_dir: str = os.path.join(ARTIFACTS_DIR,TIMESTAMP)
+
+
+
+training_pipeline_config:TrainingPipelineConfig = TrainingPipelineConfig() 
+
+
+
+@dataclass
+class DataIngestionConfig:
+    data_ingestion_dir: str = os.path.join(
+        training_pipeline_config.artifacts_dir, DATA_INGESTION_DIR_NAME
+    )
+
+    feature_store_file_path: str = os.path.join(
+        data_ingestion_dir, DATA_INGESTION_FEATURE_STORE_DIR
+    )
+
+    data_download_url: str = DATA_DOWNLOAD_URL
+
+
+
+@dataclass
+class DataValidationConfig:
+    data_validation_dir: str = os.path.join(
+        training_pipeline_config.artifacts_dir, DATA_VALIDATION_DIR_NAME
+    )
+
+    valid_status_file_dir: str = os.path.join(data_validation_dir, DATA_VALIDATION_STATUS_FILE)
+
+    required_file_list = DATA_VALIDATION_ALL_REQUIRED_FILES
+
+
+
+
+@dataclass
+class ModelTrainerConfig:
+    model_trainer_dir: str = os.path.join(
+        training_pipeline_config.artifacts_dir, MODEL_TRAINER_DIR_NAME
+    )
+
+    weight_name = MODEL_TRAINER_PRETRAINED_WEIGHT_NAME
+
+    no_epochs = MODEL_TRAINER_NO_EPOCHS
+
+    batch_size = MODEL_TRAINER_BATCH_SIZE
+
+
+
+@dataclass
+class ModelPusherConfig:
+    BUCKET_NAME: str = BUCKET_NAME
+    S3_MODEL_KEY_PATH: str = S3_MODEL_NAME
---
+++ signLanguage/pipeline/training_pipeline.py
@@ -0,0 +1,129 @@
+import sys, os
+from signLanguage.logger import logging
+from signLanguage.exception import SignException
+from signLanguage.components.data_ingestion import DataIngestion
+from signLanguage.components.data_validation import DataValidation
+from signLanguage.components.model_trainer import ModelTrainer
+from signLanguage.components.model_pusher import ModelPusher
+from signLanguage.configuration.s3_operations import S3Operation
+
+from signLanguage.entity.config_entity import (DataIngestionConfig,
+                                               DataValidationConfig,
+                                               ModelTrainerConfig,
+                                               ModelPusherConfig)
+
+from signLanguage.entity.artifact_entity import (DataIngestionArtifact,
+                                                 DataValidationArtifact,
+                                                 ModelTrainerArtifact,
+                                                 ModelPusherArtifacts)
+
+
+class TrainPipeline:
+    def __init__(self):
+        self.data_ingestion_config = DataIngestionConfig()
+        self.data_validation_config = DataValidationConfig()
+        self.model_trainer_config = ModelTrainerConfig()
+        self.model_pusher_config = ModelPusherConfig()
+        self.s3_operations = S3Operation()
+
+
+    
+    def start_data_ingestion(self)-> DataIngestionArtifact:
+        try: 
+            logging.info(
+                "Entered the start_data_ingestion method of TrainPipeline class"
+            )
+            logging.info("Getting the data from URL")
+
+            data_ingestion = DataIngestion(
+                data_ingestion_config =  self.data_ingestion_config
+            )
+
+            data_ingestion_artifact = data_ingestion.initiate_data_ingestion()
+            logging.info("Got the data from URL")
+            logging.info(
+                "Exited the start_data_ingestion method of TrainPipeline class"
+            )
+
+            return data_ingestion_artifact
+
+        except Exception as e:
+            raise SignException(e, sys)
+        
+
+    
+    def start_data_validation(
+        self, data_ingestion_artifact: DataIngestionArtifact
+    ) -> DataValidationArtifact:
+        logging.info("Entered the start_data_validation method of TrainPipeline class")
+
+        try:
+            data_validation = DataValidation(
+                data_ingestion_artifact=data_ingestion_artifact,
+                data_validation_config=self.data_validation_config,
+            )
+
+            data_validation_artifact = data_validation.initiate_data_validation()
+
+            logging.info("Performed the data validation operation")
+
+            logging.info(
+                "Exited the start_data_validation method of TrainPipeline class"
+            )
+
+            return data_validation_artifact
+
+        except Exception as e:
+            raise SignException(e, sys) from e
+        
+
+
+    def start_model_trainer(self
+    ) -> ModelTrainerArtifact:
+        try:
+            model_trainer = ModelTrainer(
+                model_trainer_config=self.model_trainer_config,
+            )
+            model_trainer_artifact = model_trainer.initiate_model_trainer()
+            return model_trainer_artifact
+
+        except Exception as e:
+            raise SignException(e, sys)
+        
+
+    
+    def start_model_pusher(self, model_trainer_artifact: ModelTrainerArtifact, s3: S3Operation):
+
+        try:
+            model_pusher = ModelPusher(
+                model_pusher_config=self.model_pusher_config,
+                model_trainer_artifact= model_trainer_artifact,
+                s3=s3
+                
+            )
+            model_pusher_artifact = model_pusher.initiate_model_pusher()
+            return model_pusher_artifact
+        except Exception as e:
+            raise SignException(e, sys)
+
+
+        
+
+    
+    def run_pipeline(self) -> None:
+        try:
+            data_ingestion_artifact = self.start_data_ingestion()
+            data_validation_artifact = self.start_data_validation(
+                data_ingestion_artifact=data_ingestion_artifact
+            )
+            if data_validation_artifact.validation_status == True:
+                model_trainer_artifact = self.start_model_trainer()
+                #model_pusher_artifact = self.start_model_pusher(model_trainer_artifact=model_trainer_artifact,s3=self.s3_operations)
+
+            
+            else:
+                raise Exception("Your data is not in correct format")
+
+        
+        except Exception as e:
+            raise SignException(e, sys)
